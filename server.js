48ae0de39bd4a5c47065037e906705ac81520730
Robin Lake <mister.lake@gmail.com>
2018-03-03 19:50:10 -0700

    new chart

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-module.exports = function () {
-  return 'hello';
-}
+const express = require('express')
+const path = require('path')
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
